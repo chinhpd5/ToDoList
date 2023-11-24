ce31bf75a4e6620365048d34ea1578120b9ed023
tuva9 <tuva9@fpt.edu.vn>
2023-11-24 22:07:46 +0700

    shopping cart

--- main.js
+++ main.js
@@ -1,78 +1,46 @@
-const form = document.getElementById('todo-form');
+const form = document.querySelector("#form")
+const fields = ["name", "price", "image"]
+const shop = document.querySelector("#shop")
+const products = [{ name: "blastoise", price: 100, image: "https://i.pinimg.com/474x/84/50/34/8450342437490d76c3027f1342caa4b9.jpg" }]
 
-const text = document.getElementById("todo-input");
-const date = document.getElementById('todo-datetime');
-
-const list = document.getElementById('todo-list');
-
-// console.log(form);
-
-//Lắng nghe sự kiện gửi dữ liệu
-
-form.addEventListener('submit', function(event){
-    //ngăn chặn sự kiện load trang
-    event.preventDefault();
-
-    //lấy giá trị từ thẻ input
-    const textInput = text.value;
-    const dateInput = date.value;
-    
-    //kiểm tra dữ liệu
-    if(textInput.trim() == ''){
-        //nếu người k nhập thông tin
-        text.classList.add('warning-text');
-        text.placeholder = 'Yêu cầu người dùng nhập thông tin';
-    }else{
-        //kiểm tra nếu tồn tại thì xóa class
-        if(text.classList.contains('warning-text')){
-            text.classList.remove('warning-text');
+form.onsubmit = function (e) {
+    e.preventDefault()
+    const formData = new FormData(form)
+    const data = {}
+    let error = false
+    for (let i = 0; i < fields.length; i++) {
+        const dataField = formData.get(fields[i])
+        const errorField = document.querySelector("#" + fields[i] + "-error")
+        errorField.innerText = ""
+        if (!dataField) {
+            errorField.innerText = "Vui lòng nhập dữ liệu"
+            error = true
         }
-
-        const liItem = document.createElement('li');
-
-        //textContent,innerText
-        const textItem = document.createElement('span');
-        //thêm nội dung vào thẻ span
-        textItem.textContent =textInput;
-        //thêm class vào thẻ
-        textItem.classList.add('task');
-        liItem.appendChild(textItem);
-
-        const dateItem = document.createElement('span');
-        dateItem.innerText = dateInput;
-        dateItem.classList.add('datetime');
-        liItem.appendChild(dateItem);
-
-        const buttonDelete = document.createElement('button');
-        buttonDelete.textContent = "Xóa";
-        buttonDelete.style.display = 'none';
-        liItem.appendChild(buttonDelete);
-
-        list.appendChild(liItem);
-
-        liItem.addEventListener('mouseover',function(event){
-            buttonDelete.style.display = 'block';
-        });
-
-        liItem.addEventListener('mouseout',function(event){
-            buttonDelete.style.display = 'none';
-        });
-
-        liItem.addEventListener('click', function(event){
-            // console.log(event.target.tagName);
-            if(event.target.tagName == 'BUTTON'){
-                //event.target.parentNode.remove();
-                liItem.remove();
-            }
-            else{
-                liItem.classList.toggle('completed');
-            }
-        });
-
-        //reset dữ liệu trong ô input
-        text.value ='';
-        date.value ='';
+        data[fields[i]] = formData.get(fields[i])
     }
+    if (!error) {
+        products.push(data)
+        form.reset()
+        render()
+        alert("Thêm mới thành công")
+    }
+}
 
+render()
 
-});
+function render() {
+    let content = ""
+    products.forEach(function (p) {
+        content += `
+        <div class="w-1/3 card">
+            <img class="w-full" src="${p.image}" alt="">
+            <div>
+                <h2>${p.name}</h2>
+                <p>${p.price}</p>
+            </div>
+            <button class="cart-btn">Add to cart</button>
+        </div>
+        `
+    })
+    shop.innerHTML = content
+}
